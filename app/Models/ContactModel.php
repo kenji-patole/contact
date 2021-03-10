471b1033103b7f6d1c8ad3a53cba8ab48ad5d3fe
kenjisupremacy <79846278+kenjisupremacy@users.noreply.github.com>
2021-03-10 12:09:11 -0400

    Paramétrage Code Igniter ContactModel

---
+++ app/Models/ContactModel.php
@@ -0,0 +1,8 @@
+<?php namespace App\Models;
+ 
+use CodeIgniter\Model;
+
+class ContactModel extends Model{
+    protected $table = 'contact';
+    protected $allowedFields = ['ContactID','ContactFirstName','ContactLastName','ContactCompany','ContactJobTitle','ContactEmail','ContactPhone','ContactNotes','ContactImage','ContactDateCreate'];
+}
